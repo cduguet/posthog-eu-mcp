0aad0f8f4cc31329baead8b4b776a10d8f154775
Cristian Duguet <duguet@gmail.com>
2025-07-13 14:31:05 +0200

    Updating for EU API support
    This drops US support

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const DEV = false;
 
-export const BASE_URL = DEV ? "http://localhost:8010" : "https://us.posthog.com";
+export const BASE_URL = DEV ? "http://localhost:8010" : "https://eu.posthog.com";
